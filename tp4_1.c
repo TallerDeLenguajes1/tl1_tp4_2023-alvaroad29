2d5c5ce787a96dde23cdd011eb544925557ade54
Alvaro Diaz <alvaroad29012001@gmail.com>
2023-04-14 22:22:02 -0300

    Add funcion BuscarTarea

--- tp4_1.c
+++ tp4_1.c
@@ -10,10 +10,22 @@ struct Tarea
     int Duracion; // entre 10 - 100
 }typedef tarea;
 
+
+//----------------------------FUNCIONES---------------------------
+
 void inicializarPunteros(tarea **tareas,int cant);
+
 void ingresarTareas(tarea **tareas,int cant);
-void listarTareas(tarea **tareas,tarea **tareasRealizadas,int cant);
+
+void moverTareas(tarea **tareas,tarea **tareasRealizadas,int cant);
+
 void mostrarTareas(tarea **tareas,int cant);
+
+void mostrarTarea(tarea tarea);
+
+tarea BuscarTarea(tarea **tareas,int cant,int idBuscado);
+
+
 int main(int argc, char const *argv[])
 {
     tarea **tareasPendientes;
@@ -21,6 +33,7 @@ int main(int argc, char const *argv[])
     int cant;
 
     // punto 1
+
     printf("Cantidad de tareas que va a realizar: ");
     scanf("%d",&cant);
     tareasPendientes = (tarea**) malloc(sizeof(tarea*) * cant); //primera reserva
@@ -31,15 +44,22 @@ int main(int argc, char const *argv[])
     inicializarPunteros(tareasPendientes,cant);
     inicializarPunteros(tareasRealizadas,cant);
     ingresarTareas(tareasPendientes,cant);
-    listarTareas(tareasPendientes,tareasRealizadas,cant);
+    mostrarTareas(tareasPendientes,cant);
+    moverTareas(tareasPendientes,tareasRealizadas,cant);
     mostrarTareas(tareasPendientes,cant);
     mostrarTareas(tareasRealizadas,cant);
 
+    //BUSQUEDA POR ID
+    int idBuscado;
+    fflush(stdin);
+    printf("Ingrese el id de la tarea que desea buscar: ");
+    scanf("%d",&idBuscado);
+    mostrarTarea(BuscarTarea(tareasPendientes,cant,idBuscado));
 
     for (int i = 0; i < cant; i++)
     {
-            free(tareasPendientes[i];)
-            free(tareasRealizadas[i];)
+            free(tareasPendientes[i]);
+            free(tareasRealizadas[i]);
     }
     free(tareasPendientes);
     free(tareasRealizadas);
@@ -81,12 +101,12 @@ void ingresarTareas(tarea **tareas,int cant)
 
 //a la hora de liberar, hago el proceso inverso, primero libero descripcion,despues tareas[i] y dsp tareas
 
-void listarTareas(tarea **tareas,tarea **tareasRealizadas,int cant)
+void moverTareas(tarea **tareas,tarea **tareasRealizadas,int cant)
 {
     int eleccion,j=0;
     for (int i = 0; i < cant; i++)
     {
-        printf("Realizo la tarea de ID: %d  (1 = SI , 0 = NO)",tareas[i]->TareaID);
+        printf("Realizo la tarea de ID: %d  (1 = SI , 0 = NO): ",tareas[i]->TareaID);
         scanf("%d",&eleccion);
         if (eleccion)
         {
@@ -96,15 +116,39 @@ void listarTareas(tarea **tareas,tarea **tareasRealizadas,int cant)
         }
     }
 }
+
+
+void mostrarTarea(tarea tarea)
+{
+    printf("ID: %d\n",tarea.TareaID);
+    printf("Duracion: %d\n",tarea.Duracion);
+    printf("Descripcion: %s\n",tarea.Descripcion);
+    printf("---------------------------\n");
+}
+
 void mostrarTareas(tarea **tareas,int cant)
 {
+    printf("\n==============MOSTRAR TAREAS=================\n");
     for (int i = 0; i < cant; i++)
     {
         if (tareas[i] != NULL)
         {
-            printf("ID: %d\n",tareas[i]->TareaID);
-            printf("Duracion: %d\n",tareas[i]->Duracion);
-            printf("%s\n",tareas[i]->Descripcion);
+            mostrarTarea(*tareas[i]); // le mando la struct, si mando tareas[i] seria un puntero entonces mostrarTarea quedaria void mostrarTarea(tarea *tarea) y tendria que usar el operador flecha
         }
     }
+}
+
+
+tarea BuscarTarea(tarea **tareas,int cant,int idBuscado)
+{
+
+    int i;
+    for (int i = 0; i < cant; i++)
+    {
+        if (tareas[i]->TareaID == idBuscado)
+        {
+            return *tareas[i]; //devuelvo el struct NO el puntero
+        }
+    }
+
 }
